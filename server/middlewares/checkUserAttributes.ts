a10d3e027d3f43778c601520a9d55bc964d38cce
Andrés Siri <andres.siri@hotmail.com>
2023-03-25 14:02:16 -0300

    TH-3 Create check user attributes middleware

---
+++ server/middlewares/checkUserAttributes.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+
+const checkUserAttributes = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const typesDictionary = {
+    firstName: "string",
+    lastName: "string",
+    verified: "boolean",
+    RoleId: "role",
+  };
+
+  const arrayOfValidAttributes: string[] = req.params.id
+    ? ["firstName", "lastName", "verified", "RoleId"]
+    : ["firstName", "lastName"];
+
+  const bodyAttributes = Object.keys(req.body);
+
+  if (!bodyAttributes.length) {
+    res.status(400);
+    throw new Error("Please send any field for update");
+  }
+
+  bodyAttributes.forEach((attribute: string) => {
+    if (!arrayOfValidAttributes.includes(attribute)) {
+      res.status(400);
+      throw new Error("Please send valid user fields");
+    }
+    const typeToCheck =
+      typesDictionary[attribute as keyof typeof typesDictionary];
+
+    if (typeToCheck === "string") return;
+
+    if (
+      typeToCheck === "boolean" &&
+      ["true", "false"].includes(req.body[attribute])
+    )
+      return;
+
+    if (typeToCheck === "role" && ["1", "2"].includes(req.body[attribute]))
+      return;
+
+    res.status(400);
+    throw new Error(`Please send a valid value for field '${attribute}'`);
+  });
+
+  next();
+};
+
+export default checkUserAttributes;
