ec2bac00a69d6f14eeb58004fe9f65b4ec86777b
Andrés Siri <andres.siri@hotmail.com>
2023-03-25 09:33:39 -0300

    TH-3 Create request time limiter helper

--- server/helpers/index.ts
+++ server/helpers/index.ts
@@ -1,3 +1,4 @@
 export { default as createToken } from "./createToken";
 export { default as hashPassword } from "./hashPassword";
+export { default as requestTimeLimiter } from "./requestTimeLimiter";
 export * from "./validatorChecks";
