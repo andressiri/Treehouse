9b6ffa1c630ee23d74618fd7af7bd4b379ff5eb6
Andrés Siri <andres.siri@hotmail.com>
2023-03-29 01:37:42 -0300

    TH-8 Implement initial elements disposition

--- client/components/Templates/HeadSetup/index.tsx
+++ client/components/Templates/HeadSetup/index.tsx
@@ -4,7 +4,10 @@ const HeadSetup = () => {
   return (
     <Head key="customHeadComponent">
       <title>Treehouse</title>
-      <meta name="description" content="Treehouse app" />
+      <meta
+        name="description"
+        content="Treehouse: An app developed by Andrés Siri to solve the Rather Labs challenge"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
--- client/components/Templates/Layout/styledComponents.ts
+++ client/components/Templates/Layout/styledComponents.ts
@@ -5,15 +5,29 @@ export const Container = styled(Box)(() => ({
   position: "relative",
   width: "100%",
   height: "100vh",
-  paddingLeft: "320px",
+  padding: "120px 0px 0px 320px",
 }));
-export const Sidebar = styled(Box)(({ theme }) => ({
+
+export const Sidebar = styled("nav")(({ theme }) => ({
   position: "fixed",
   top: "0px",
-  right: "0px",
   bottom: "0px",
+  left: "0px",
   width: "320px",
   height: "100vh",
   background: theme.palette.primary.main,
   color: theme.palette.primary.contrastText,
+}));
+
+export const Header = styled("header")(({ theme }) => ({
+  position: "absolute",
+  top: "0px",
+  left: "320px",
+  width: "calc(100% - 320px)",
+  height: "120px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  background: "transparent",
+  color: theme.palette.primary.main,
 }));
