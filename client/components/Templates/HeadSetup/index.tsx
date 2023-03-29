bbe04a3bafcdbb435caedaa30b88499ac253e158
Andrés Siri <andres.siri@hotmail.com>
2023-03-29 02:49:23 -0300

    TH-9 Fix favicon

--- client/components/Templates/HeadSetup/index.tsx
+++ client/components/Templates/HeadSetup/index.tsx
@@ -9,7 +9,24 @@ const HeadSetup = () => {
         content="Treehouse: An app developed by Andrés Siri to solve the Rather Labs challenge"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
     </Head>
   );
 };
