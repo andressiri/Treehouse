85120c56248e14cb53c589809ad8bfca490a2c1e
Andrés Siri <andres.siri@hotmail.com>
2023-03-29 09:37:30 -0300

    TH-9 Create styled button

---
+++ client/typings/buttons.d.ts
@@ -0,0 +1,25 @@
+import { SxProps } from "@mui/system";
+
+export interface IStyledButton {
+  BGType?:
+    | "primaryBG"
+    | "secondaryBG"
+    | "primaryContrastBG"
+    | "secondaryContrastBG"
+    | "primaryOutlined"
+    | "secondaryOutlined"
+    | "primaryContrastOutlined"
+    | "secondaryContrastOutlined";
+  transparent?: boolean;
+  hover?: "primary" | "secondary" | "outlined";
+  passRef?: RefObject<HTMLButtonElement>;
+  sx?: SxProps;
+  startIcon?: JSX.Element;
+  endIcon?: JSX.Element;
+  shadow?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  onFocus?: MouseEventHandler<HTMLButtonElement>;
+  onBlur?: MouseEventHandler<HTMLButtonElement>;
+  tabIndex?: number;
+  children?: string | JSX.Element | JSX.Element[];
+}
