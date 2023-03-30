cc5934b14c13a790a907d64c3e30bb719c2df537
Andrés Siri <andres.siri@hotmail.com>
2023-03-30 08:28:50 -0300

    TH-10 Create hook to resolve unhandled errors

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,8 +2,11 @@ import { FC } from "react";
 import { IndexPageTitle } from "../components/Molecules";
 import { RoomCardsDisplay } from "../components/Organisms";
 import { Layout } from "../components/Templates";
+import { useResolveUnhandledErrors } from "../utils/hooks";
 
 const Home: FC = () => {
+  useResolveUnhandledErrors();
+
   return (
     <Layout>
       <IndexPageTitle />
---
+++ client/utils/hooks/useResolveUnhandledErrors/index.ts
@@ -0,0 +1,7 @@
+import useResolveRoomsErrors from "./useResolveRoomsErrors";
+
+const useResolveUnhandledErrors = () => {
+  useResolveRoomsErrors();
+};
+
+export default useResolveUnhandledErrors;
