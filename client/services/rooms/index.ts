d79ee9f7d38584122af6392e76f16f4d565e1c69
Andrés Siri <andres.siri@hotmail.com>
2023-03-30 19:59:32 -0300

    TH-10 Create delete room service

--- client/services/rooms/index.ts
+++ client/services/rooms/index.ts
@@ -1,3 +1,4 @@
+export { default as useDeleteRoom } from "./useDeleteRoom";
 export { default as useGetRoomById } from "./useGetRoomById";
 export { default as useGetRoomByIdEffect } from "./useGetRoomByIdEffect";
 export { default as useGetRoomsWithRelations } from "./useGetRoomsWithRelations";
