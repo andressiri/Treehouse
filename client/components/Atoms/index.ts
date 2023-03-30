bd1d45b473989b0ef30ddc7743cf450bf078a16a
Andrés Siri <andres.siri@hotmail.com>
2023-03-30 19:09:01 -0300

    TH-10 Create section title component

--- client/components/Atoms/index.ts
+++ client/components/Atoms/index.ts
@@ -1,6 +1,7 @@
 export { default as ActionIconButton } from "./ActionIconButton";
 export { default as BackgroundTexture } from "./BackgroundTexture";
 export { default as LinkIconButton } from "./LinkIconButton";
+export { default as SectionTitle } from "./SectionTitle";
 export { default as Spinner } from "./Spinner";
 export { default as StyledButton } from "./StyledButton";
 export { default as Tooltip } from "./Tooltip";
