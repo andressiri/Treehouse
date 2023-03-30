69ce94cac4193090480383000eed059616688046
Andrés Siri <andres.siri@hotmail.com>
2023-03-30 11:15:31 -0300

    TH-10 Create person basic info component

--- client/components/Molecules/index.ts
+++ client/components/Molecules/index.ts
@@ -2,6 +2,7 @@ export { default as FloatingOpenMenu } from "./FloatingOpenMenu";
 export { default as IndexPageTitle } from "./IndexPageTitle";
 export { default as NavLinks } from "./NavLinks";
 export { default as NavLogo } from "./NavLogo";
+export { default as PersonBasicInfo } from "./PersonBasicInfo";
 export { default as RoomCardDescription } from "./RoomCardDescription";
 export { default as RoomCardTeacher } from "./RoomCardTeacher";
 export { default as RoomCardTitle } from "./RoomCardTitle";
