07ac58459cae7225133222a6b3c0217458c98ed5
Andrés Siri <andres.siri@hotmail.com>
2023-03-30 08:01:03 -0300

    TH-10 Fix axios instance

--- client/utils/helpers/axiosInstance/index.ts
+++ client/utils/helpers/axiosInstance/index.ts
@@ -2,6 +2,14 @@ import axios from "axios";
 import { v4 } from "uuid";
 import { API_ORIGIN, API_ROUTE, API_VERSION } from "../../../config/constants";
 
+interface ErrorWithResponse extends Error {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
+
 const axiosInstance = async (endpoint: string, data = {}, method = "GET") => {
   // const user = JSON.parse(localStorage.getItem("user"));
   // const temporaryToken = JSON.parse(sessionStorage.getItem("temporaryToken"));
@@ -20,22 +28,30 @@ const axiosInstance = async (endpoint: string, data = {}, method = "GET") => {
     (formData as FormData).append("image", file, `${v4()}.jpeg`);
   }
 
-  const response = await axios({
-    method,
-    url: `${API_ORIGIN}${API_ROUTE}${API_VERSION}${endpoint}`,
-    data: formData,
-    headers: {
-      // authorization: `Bearer ${token}`,
-      "Content-Type": contentType,
-    },
-  });
+  try {
+    const response = await axios({
+      method,
+      url: `${API_ORIGIN}${API_ROUTE}${API_VERSION}${endpoint}`,
+      data: formData,
+      headers: {
+        // authorization: `Bearer ${token}`,
+        "Content-Type": contentType,
+      },
+    });
 
-  if (response.status >= 400) {
-    throw new Error(`${response}`);
+    sessionStorage.removeItem("file");
+    return response.data;
+  } catch (err) {
+    if (
+      err instanceof Error &&
+      (err as ErrorWithResponse).response?.data?.message
+    ) {
+      throw new Error((err as ErrorWithResponse).response.data.message);
+    } else {
+      console.log("Unexpected error", err); // eslint-disable-line no-console
+      throw new Error("Unexpected error");
+    }
   }
-
-  sessionStorage.removeItem("file");
-  return response.data;
 };
 
 export default axiosInstance;
