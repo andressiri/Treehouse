e1cb174d6ff1e8eef1ccea70ba83f1d5fe5243b6
Andrés Siri <andres.siri@hotmail.com>
2023-03-31 06:48:02 -0300

    TH-10 Create edit room service

--- client/services/rooms/index.ts
+++ client/services/rooms/index.ts
@@ -1,5 +1,6 @@
 export { default as useCreateRoom } from "./useCreateRoom";
 export { default as useDeleteRoom } from "./useDeleteRoom";
+export { default as useEditRoom } from "./useEditRoom";
 export { default as useGetRoomById } from "./useGetRoomById";
 export { default as useGetRoomByIdEffect } from "./useGetRoomByIdEffect";
 export { default as useGetRoomsWithRelations } from "./useGetRoomsWithRelations";
