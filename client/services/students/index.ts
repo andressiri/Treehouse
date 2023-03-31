1b48768dbead741220bb93a0eb6b8bf6b8d5e1f4
Andrés Siri <andres.siri@hotmail.com>
2023-03-31 09:29:18 -0300

    TH-12 Create delete student service

--- client/services/students/index.ts
+++ client/services/students/index.ts
@@ -1,3 +1,4 @@
+export { default as useDeleteStudent } from "./useDeleteStudent";
 export { default as useGetStudentById } from "./useGetStudentById";
 export { default as useGetStudentByIdEffect } from "./useGetStudentByIdEffect";
 export { default as useGetStudentsWithRelations } from "./useGetStudentsWithRelations";
