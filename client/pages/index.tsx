b2a8754f43f9f19ffea792aaa2f9285134aab702
Andrés Siri <andres.siri@hotmail.com>
2023-04-24 12:09:35 -0300

    TH-16 Set get static props for landing page pre-render

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,24 +5,49 @@ import { useResolveUnhandledErrors } from "../utils/hooks";
 import { IndexPageTitle } from "../components/Molecules";
 import { CardsDisplay } from "../components/Organisms";
 import { Layout } from "../components/Templates";
+import {
+  API_ORIGIN,
+  API_ROUTE,
+  API_VERSION,
+  WITH_RELATIONS,
+  ROOMS_ROUTE,
+} from "../config/constants";
+import { GetStaticProps } from "next";
 
-const Home: FC = () => {
-  const { rooms, isLoading } = useContext(RoomsContext);
+interface Props {
+  staticRooms?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
+const Home: FC<Props> = ({ staticRooms }) => {
+  const { rooms } = useContext(RoomsContext);
+
   useGetRoomsWithRelationsEffect();
   useResolveUnhandledErrors();
 
   return (
     <Layout>
-      {isLoading ? (
-        <></>
-      ) : (
-        <main>
-          <IndexPageTitle />
-          <CardsDisplay displayArray={rooms} modelName="room" />
-        </main>
-      )}
+      <main>
+        <IndexPageTitle />
+        <CardsDisplay
+          displayArray={!rooms[0] ? staticRooms : rooms}
+          modelName="room"
+        />
+      </main>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    `${API_ORIGIN}/${API_ROUTE}/${API_VERSION}/${ROOMS_ROUTE}/${WITH_RELATIONS}`
+  );
+  const staticRooms = await res.json();
+
+  return {
+    props: {
+      staticRooms,
+    },
+  };
 };
 
 export default Home;
