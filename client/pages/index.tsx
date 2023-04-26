93cce2034d733f5b311fdedaa3ba90e22f8352b1
Andrés Siri <andres.siri@hotmail.com>
2023-04-26 16:15:04 -0300

    TH-16 Handle landing page rooms request response

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,9 @@
 import { FC, useContext } from "react";
 import { RoomsContext } from "../contexts";
-import { useGetRoomsWithRelationsEffect } from "../services";
-import { useResolveUnhandledErrors } from "../utils/hooks";
+import {
+  useGetRoomsWithRelationsEffect,
+  useHandleRoomsResponseEffect,
+} from "../services";
 import { IndexPageTitle } from "../components/Molecules";
 import { CardsDisplay } from "../components/Organisms";
 import { Layout } from "../components/Templates";
@@ -22,7 +24,7 @@ const Home: FC<Props> = ({ staticRooms }) => {
   const { rooms } = useContext(RoomsContext);
 
   useGetRoomsWithRelationsEffect();
-  useResolveUnhandledErrors();
+  useHandleRoomsResponseEffect({});
 
   return (
     <Layout>
