79f3ddd02f8b073ccffdefc0666d53826016eaac
Andrés Siri <andres.siri@hotmail.com>
2023-04-30 11:51:39 -0300

    TH-16 Create general constants

---
+++ client/config/constants/general.ts
@@ -0,0 +1,20 @@
+export const ROOM_ENTITY = "room";
+export const STUDENT_ENTITY = "student";
+export const TEACHER_ENTITY = "teacher";
+
+export const FEMALE_GENDER = "female";
+export const MALE_GENDER = "male";
+export const INTERSEX_GENDER = "intersex";
+export const TRANS_GENDER = "trans";
+export const NON_CONFORMING_GENDER = "non-conforming";
+export const PERSONAL_GENDER = "personal";
+export const EUNUCH_GENDER = "eunuch";
+export const GENDER_ARRAY_GENDER = [
+  FEMALE_GENDER,
+  MALE_GENDER,
+  INTERSEX_GENDER,
+  TRANS_GENDER,
+  NON_CONFORMING_GENDER,
+  PERSONAL_GENDER,
+  EUNUCH_GENDER,
+];
--- client/config/constants/index.ts
+++ client/config/constants/index.ts
@@ -1,3 +1,4 @@
+export * from "./general";
 export * from "./routingAPI";
 export * from "./serviceContextInitialValues";
 export * from "./socialMediaURLs";
