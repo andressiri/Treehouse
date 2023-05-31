4ed82fc42b5dc54b8e9af73fd9e230c4cdc8aede
Andrés Siri <andres.siri@hotmail.com>
2023-05-31 20:36:11 -0300

    TH-18 Improve form basic response handlers hook

--- client/utils/hooks/generalFormsHooks/useGetFormBasicResponseHandlers/index.ts
+++ client/utils/hooks/generalFormsHooks/useGetFormBasicResponseHandlers/index.ts
@@ -20,7 +20,7 @@ import {
   TEACHERS_SINGULAR,
 } from "../../../../config/constants";
 
-const useGetFormBasicResponseHandlers = (entity: Entities) => {
+const useGetFormBasicResponseHandlers = (entity?: Entities) => {
   const { room } = useContext(RoomsContext);
   const { student } = useContext(StudentsContext);
   const { teacher, teacherWithRelations } = useContext(TeachersContext);
@@ -28,16 +28,17 @@ const useGetFormBasicResponseHandlers = (entity: Entities) => {
   const [keepLoading, setKeepLoading] = useState(false);
   const { push } = useRouter();
 
-  const route =
-    entity === ROOM_ENTITY
-      ? `/${ROOMS_ROUTE}/${ROOMS_SINGULAR}/${(room as IRoom)?.id}`
-      : entity === STUDENT_ENTITY
-      ? `/${STUDENTS_ROUTE}/${STUDENTS_SINGULAR}/${(student as IStudent)?.id}`
-      : `/${TEACHERS_ROUTE}/${TEACHERS_SINGULAR}/${
-          (teacher as ITeacher)?.id
-            ? (teacher as ITeacher)?.id
-            : (teacherWithRelations as ITeacherWithRelations)?.id
-        }`;
+  const route = !entity
+    ? `/`
+    : entity === ROOM_ENTITY
+    ? `/${ROOMS_ROUTE}/${ROOMS_SINGULAR}/${(room as IRoom)?.id}`
+    : entity === STUDENT_ENTITY
+    ? `/${STUDENTS_ROUTE}/${STUDENTS_SINGULAR}/${(student as IStudent)?.id}`
+    : `/${TEACHERS_ROUTE}/${TEACHERS_SINGULAR}/${
+        (teacher as ITeacher)?.id
+          ? (teacher as ITeacher)?.id
+          : (teacherWithRelations as ITeacherWithRelations)?.id
+      }`;
 
   const errorAction = (message: string) => {
     setErrorMessage(message);
