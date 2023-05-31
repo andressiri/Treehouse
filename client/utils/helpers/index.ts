60575c4d72f9b1145f7987c46ed20e8aa618792d
Andrés Siri <andres.siri@hotmail.com>
2023-05-31 20:34:33 -0300

    TH-18 Create email validation helper

--- client/utils/helpers/index.ts
+++ client/utils/helpers/index.ts
@@ -5,4 +5,5 @@ export { default as getWindowDimensions } from "./getWindowDimensions";
 export { default as sanitizeFormChanges } from "./sanitizeFormChanges";
 export { default as sanitizeRequestData } from "./sanitizeRequestData";
 export { default as sortByName } from "./sortByName";
+export { default as validateEmail } from "./validateEmail";
 export * from "./requestHandlers";
