1c42feadf6ba4e7cac1b697632bfb88238dc31c9
Andrés Siri <andres.siri@hotmail.com>
2023-06-03 02:36:47 -0300

    TH-18 Create handle auth user effect hook

--- client/utils/hooks/authHooks/index.ts
+++ client/utils/hooks/authHooks/index.ts
@@ -1,1 +1,2 @@
+export { default as useHandleAuthUserEffects } from "./useHandleAuthUserEffects";
 export * from "./register";
---
+++ client/utils/hooks/authHooks/useHandleAuthUserEffects/index.ts
@@ -0,0 +1,51 @@
+import { useCallback, useContext, useEffect } from "react";
+import { UsersContext } from "../../../../contexts";
+import { IAuthUser } from "../../../../typings/users";
+
+const useHandleAuthUserEffects = () => {
+  const { authUser, remember } = useContext(UsersContext);
+  const windowAvailable = typeof window !== "undefined";
+
+  const clearUser = useCallback(() => {
+    localStorage.removeItem("authUser");
+    localStorage.removeItem("token");
+    localStorage.removeItem("rememberUser");
+    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  }, []);
+
+  const addBeforeUnloadEvent = useCallback(() => {
+    if (windowAvailable) {
+      window.addEventListener("beforeunload", (e) => {
+        e.preventDefault();
+
+        clearUser();
+      });
+    }
+  }, [windowAvailable, clearUser]);
+
+  useEffect(() => {
+    addBeforeUnloadEvent();
+  }, [addBeforeUnloadEvent]);
+
+  useEffect(() => {
+    if ((authUser as IAuthUser).token && windowAvailable) {
+      localStorage.setItem("authUser", JSON.stringify(authUser));
+      localStorage.setItem("token", (authUser as IAuthUser)?.token);
+
+      sessionStorage.setItem("authUser", JSON.stringify(authUser));
+      sessionStorage.setItem("token", (authUser as IAuthUser)?.token);
+
+      document.cookie = `token=${
+        (authUser as IAuthUser)?.token
+      }; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+
+      if (remember) localStorage.setItem("rememberUser", "true");
+
+      return;
+    }
+
+    if (windowAvailable) clearUser();
+  }, [authUser, remember, windowAvailable, clearUser]);
+};
+
+export default useHandleAuthUserEffects;
